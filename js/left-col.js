9834ac18c37faeb5d69d13e6e724a35b2d74672a
xihuanlei <389580903@qq.com>
2020-11-03 09:24:17 +0800

    Site updated: 2020-11-03 09:24:13

---
+++ js/left-col.js
@@ -0,0 +1,15 @@
+<div id="he-plugin-standard"></div>
+<script>
+WIDGET = {
+  CONFIG: {
+    "layout": 2,
+    "width": "230",
+    "height": "270",
+    "background": 1,
+    "dataColor": "B8E986",
+    "borderRadius": 5,
+    "key": "49f4a71c7e2e4eba956371650749150f"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/standard/static/js/he-standard-common.js?v=2.0"></script>
